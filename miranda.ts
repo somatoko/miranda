ba6b49f15392b1cabd7d94c8b1e2a4d45b0ce32e
somatoko <frostmind@yandex.ru>
2023-01-26 17:37:49 +0100

    update to installable script

--- miranda.ts
+++ miranda.ts
@@ -1,9 +1,9 @@
-#!/usr/bin/env -S deno run --allow-net --allow-read --allow-write --allow-env --allow-run
-
 import { CliParser } from "./miranda/cli-parser.ts";
 import { Config } from "./miranda/config-loader.ts";
 import { MigrationManager } from "./miranda/migrator.ts";
 
-await Config.load()
-await MigrationManager.loadFromFiles()
-await CliParser.parseAndRun()
+if (import.meta.main) {
+  await Config.load()
+  await MigrationManager.loadFromFiles()
+  await CliParser.parseAndRun()
+}
